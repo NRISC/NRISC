2de8c3ee8c915d33563f9622e519b8f4935d72be
MarianoBW <marianoberwangerwille@gmail.com>
2017-03-11 16:15:06 -0300

    errrrrrooooouuuuuuu

--- MEM_TB.v
+++ MEM_TB.v
@@ -3,12 +3,11 @@
 /*************************************************************************
  *  descricao do testbench da MEM                   versao 2.02          *
  *                                                                       *
- *  Developer: Marlon                              15-12-2016            *
- *             marlonsigales@gmail.com                                   *                                     *
- *  Corrector: 
- *             Mariano                             15-12-2016            *
- *             marianobw@hotmail.com
- *			   Jean Carlos Scheunemann             15-12-2016            *
+ *  Developer: Mariano                              11-03-2017           *
+ *             marianobw@hotmail.com                                     *                                     
+ *  Corrector: Marlon                              15-12-2016            *
+ *             marlonsigales@gmail.com                                   *
+ *	           Jean Carlos Scheunemann             15-12-2016            *
  *             jeancarsch@gmail.com                                      *
  *                                                                       *
  *************************************************************************/ 
@@ -27,27 +26,27 @@
 
  reg [0:TAM-1]		dataIN0;
  reg [0:TAM-1]		dataIN1;
- reg [0:TAM-1]		dataOUT0;
- reg [0:TAM-1]		dataOUT1;
+ wire [0:TAM-1]		dataOUT0;
+ wire [0:TAM-1]		dataOUT1;
  reg [0:TAM-1]		dataADDR0;
  reg [0:TAM-1]		dataADDR1;
  reg [0:Ncores-1] 	dataWrite;
  reg [0:Ncores-1] 	dataLoad;
 
- reg
  
  
- DataMEM #(.Ncores(Ncores), .Lmem(Lmem), .TAM(TAM)) dut (
-				dataIN0(dataIN0),
-				dataIN1(dataIN1),
-				dataOUT0(dataOUT0),
-				dataOUT1(dataOUT1),
-				dataADDR0(dataADDR0),
-				dataADDR1(dataADDR1),
-				dataLoad(dataLoad),
-				dataWrite(dataWrite),
-				clk(clk),
-				rst(rst)
+ DataMEM #(.Ncores(Ncores), .Lmem(Lmem), .TAM(TAM)) dut
+				(
+				.dataIN0(dataIN0),
+				.dataIN1(dataIN1),
+				.dataOUT0(dataOUT0),
+				.dataOUT1(dataOUT1),
+				.dataADDR0(dataADDR0),
+				.dataADDR1(dataADDR1),
+				.dataLoad(dataLoad),
+				.dataWrite(dataWrite),
+				.clk(clk),
+				.rst(rst)
 				);
  
  
@@ -59,73 +58,76 @@ initial dataADDR0 = 0;
 initial dataADDR1 = 0;
 initial dataIN0 = 1;
 initial dataIN1 = 2;
-initial dataLoad = {Ncores{1'b1}};
+initial dataLoad = 0;
 initial dataWrite = 0;
 
-
+reg x;
+initial x=0;
+reg i;
+initial i=0;
 always #(PERIOD/2) clk = ~clk;
  
  
-always @(message)      //sempre que houver mensagem troca as variáveis e mostra se é ok ou não
+always @(x)      //sempre que houver mensagem troca as variáveis e mostra se é ok ou não
     begin
 	
-	$display (" %s ", message);         
+	//$display (" %s ", message);         
     //$stop;  
 	
 	
         /*random comand sintax:
         min + {$random(seed)}%(max-min+1) or can use $dist_uniform(seed, min, max) */
 	
-		dataIN0   <=    {tam{1'b0}} + {$random()}%((tam){1'b1});
-		dataIN0   <=    {tam{1'b0}} + {$random()}%((tam){1'b1});
-		dataADDR0 <=    {tam{1'b0}} + {$random()}%((tam){1'b1});
-		dataADDR1 <=    {tam{1'b0}} + {$random()}%((tam){1'b1});
-		dataLoad  <= {Ncores{1'b0}} + {$random()}%((Ncores){1'b1});
-		dataWrite <= {Ncores{1'b0}} + {$random()}%((Ncores){1'b1});
+		dataIN0   <= {$random()}%(17'b11111111111111111);
+		dataIN1   <= {$random()}%(17'b11111111111111111);
+		dataADDR0 <= {$random()}%(8'b11111111);
+		dataADDR1 <= {$random()}%(8'b11111111);
+		//dataLoad  <= {$random()}%(17'b11111111111111111);
+		//dataWrite <= {$random()}%(17'b11111111111111111);
 	   
     end 
  
 /*==========================================================*/ 
  
- 
- 
-/*-------------------- testes ---------------------------*/
-initial      
-//  verifica se oque foi enviado foi escrito... e lido 
-    begin 
-	
-	for (i=0; i<200; i++) begin
-	
-			@(posedge clk)
-				dataWrite=~dataWrite;
-				dataLoad=~dataLoad;
-				
-			@(posedge clk)
-				dataWrite=~dataWrite;
-				dataLoad=~dataLoad;
-						
-			if ((dataOUT0 == dataIN0) && (dataOUT1 == dataIN1)) begin
-				message = "escrita e leitura teste ok" , $time, "unidade de tempo";
-			end
-			else begin
-				if ((dataOUT0 == dataIN0)) 
-					message = "data0 com erro", $time, "unidade de tempo";
-				if ((dataOUT1 == dataIN1)) 
-					message = "data1 com erro", $time, "unidade de tempo";	
-			end
-			
-			dataADDR0=dataADDR0+1;
-			dataADDR1=dataADDR1+1;
-			@(posedge clk);
-			if ((dataOUT0 == dataIN0) && (dataOUT1 == dataIN1))
-				message = "pode ser que está escrevendo em toda memória a mesma coisa" , $time, "unidade de tempo";
+ always @(posedge clk)begin
+	if (i==0)begin
+		dataWrite=2'b11;
+		dataLoad=2'b00;	
+		i=1;
+	end else if (i==1)begin
+		dataWrite=2'b00;
+		dataLoad=2'b11;
+		i=0;
 	end
-	
-   
-  end 
+ end
+/*-------------------- testes ---------------------------*/
 
-
-
+ always@(negedge clk) begin	
+	if(i==0)begin
+		if ((dataOUT0 == dataIN0) && (dataOUT1 == dataIN1)) begin
+			$display("escrita e leitura teste ok" , $time, "unidade de tempo");
+			x=~x;
+		end else begin
+			if ((dataOUT0 == dataIN0))begin 
+				$display("data0 com erro", $time, "unidade de tempo");
+				x=~x;
+				end
+			if ((dataOUT1 == dataIN1))begin
+				$display("data1 com erro", $time, "unidade de tempo");	
+				x=~x;
+				end
+			
+		end
+ 	//dataADDR0=dataADDR0+1;
+ 	//dataADDR1=dataADDR1+1;
+ 	//if ((dataOUT0 == dataIN0) && (dataOUT1 == dataIN1))begin
+ 	//	$display("pode ser que está escrevendo em toda memória a mesma coisa" , $time, "unidade de tempo");
+ 	//	x=~x;
+ 	end
+ 
+ 
+ 
+ end 
 
  
 endmodule 
