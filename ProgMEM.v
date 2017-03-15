dc3a019b9ba74210eb79436ba1e4bba73b6f8c42
JCScheunemann <scheunemanngame@gmail.com>
2017-03-15 00:16:48 -0300

    paremetrização do arquivo de entrada da memoria de programa

--- ProgMEM.v
+++ ProgMEM.v
@@ -7,8 +7,10 @@ module progMEM(
                 );
     parameter TAM=16;
     parameter Lmem=8;
+    parameter ProgFile= "Prog.hex";
 
-    input wire [0:TAM-1] progADDR;
+
+    input wire [TAM-1:0] progADDR;
     input wire [1:0] CoreStatus;
 
     input wire rst;
@@ -19,11 +21,11 @@ module progMEM(
     reg [15:0] MainMEM [0:(1<<Lmem)-1];
     reg [1:0] lastCoreStatus;
 
-    initial $readmemh("Prog.hex", MainMEM); //le o arquivo hexadecimal que contem o programa;
+    initial $readmemh(ProgFile, MainMEM); //le o arquivo hexadecimal que contem o programa;
 
     always @ ( negedge clk ) begin
         lastCoreStatus=CoreStatus;
         if(CoreStatus==2'b00)
-            Instruction=(lastCoreStatus==2'b00 || lastCoreStatus==2'b01) ? MainMEM[progADDR[0:Lmem]+1]:MainMEM[progADDR[0:Lmem]];
+            Instruction=(lastCoreStatus==2'b00 || lastCoreStatus==2'b01) ? MainMEM[progADDR[Lmem:0]+1]:MainMEM[progADDR[Lmem:0]];
     end
 endmodule
